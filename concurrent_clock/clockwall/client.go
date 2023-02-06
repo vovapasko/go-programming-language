195096f5b6952779ee55ed6b1810f51e47241156
vovapasko <vovapasko1699@gmail.com>
2023-02-06 23:39:24 +0100

    made a template for client.go

--- concurrent_clock/clockwall/client.go
+++ concurrent_clock/clockwall/client.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
 
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatal("Could not close a connection")
+		}
+	}(conn)
+	if err != nil {
+		log.Fatal("Connection lost")
+	}
+	handleOutput(conn)
+}
+
+func handleOutput(conn net.Conn) {
+	_, err := io.Copy(os.Stdout, conn)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
