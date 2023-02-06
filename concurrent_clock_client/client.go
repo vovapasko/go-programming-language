840caf53b818d2b24735a7d7a3145cf7e10617a9
vovapasko <vovapasko1699@gmail.com>
2023-02-06 14:29:42 +0100

    provided timeclock client

--- concurrent_clock_client/client.go
+++ concurrent_clock_client/client.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
 
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal("Connection lost")
+	}
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatal("Something happened with the connection")
+		}
+	}(conn)
+	giveOutput(conn)
+}
+
+func giveOutput(conn net.Conn) {
+	_, err := io.Copy(os.Stdout, conn)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
