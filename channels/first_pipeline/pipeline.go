3a993b9c620f9796596a2ecd8a5521de74d64858
vovapasko <vovapasko1699@gmail.com>
2023-02-07 13:56:26 +0100

    added sequential 3d surface computation task

--- channels/first_pipeline/pipeline.go
+++ channels/first_pipeline/pipeline.go
@@ -9,20 +9,34 @@ func main() {
 	// Number generator -> Squarer -> Printer
 	numbers := make(chan int)
 	squares := make(chan int)
-	go func() {
-		for x := 0; x < 50; x++ {
-			numbers <- x
-		}
-		close(numbers)
-	}()
-	go func() {
-		for number := range numbers {
-			squares <- number * number
-		}
-		close(squares)
-	}()
-	for square := range squares {
+	go counter(numbers)
+	go squarer(numbers, squares)
+
+	// method 1
+	//go printer(squares)
+	//time.Sleep(time.Second)
+
+	// or just start in main goroutine
+	printer(squares)
+}
+
+func printer(in <-chan int) {
+	for square := range in {
 		fmt.Println(square)
 		time.Sleep(10 * time.Millisecond)
 	}
+}
+
+func counter(out chan<- int) {
+	for x := 0; x < 50; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func squarer(in <-chan int, out chan<- int) {
+	for number := range in {
+		out <- number * number
+	}
+	close(out)
 }
---
+++ concurrent_echo_server/server1/reverb2.go
@@ -0,0 +1,51 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 223.
+
+// Reverb1 is a TCP server that simulates an echo.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+// !+
+func echo2(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn2(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo2(c, input.Text(), 2*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+//!-
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn2(conn)
+	}
+}
