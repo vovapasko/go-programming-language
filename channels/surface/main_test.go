131edd8eaa84e3f3e31bbef49d033f8166f6c17e
vovapasko <vovapasko1699@gmail.com>
2023-02-07 16:37:42 +0100

    added benchmark for concurrent svg function

--- channels/surface/main_test.go
+++ channels/surface/main_test.go
@@ -6,9 +6,18 @@ import (
 )
 
 func BenchmarkSVG(b *testing.B) {
-	buff := &bytes.Buffer{}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		SVG(buff)
-	}
+	b.Run("benchmark syncron svg", func(b *testing.B) {
+		buff := &bytes.Buffer{}
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			SVG(buff)
+		}
+	})
+	b.Run("benchmark concurrent svg", func(b *testing.B) {
+		buff := &bytes.Buffer{}
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			SVG2(buff)
+		}
+	})
 }
