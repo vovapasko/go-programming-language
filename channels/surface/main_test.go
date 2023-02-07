e0209a82ae1fd2f247296ac94635ef9213553b51
vovapasko <vovapasko1699@gmail.com>
2023-02-07 16:52:37 +0100

    provided approval tests for svg functions

--- channels/surface/main_test.go
+++ channels/surface/main_test.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"bytes"
+	approvals "github.com/approvals/go-approval-tests"
 	"testing"
 )
+
+func TestSVG(t *testing.T) {
+	t.Run("run sync svg function", func(t *testing.T) {
+		buff := &bytes.Buffer{}
+		SVG(buff)
+		approvals.VerifyString(t, buff.String())
+	})
+	t.Run("run concurrent svg function", func(t *testing.T) {
+		buff := &bytes.Buffer{}
+		SVG2(buff)
+		approvals.VerifyString(t, buff.String())
+	})
+}
 
 func BenchmarkSVG(b *testing.B) {
 	b.Run("benchmark syncron svg", func(b *testing.B) {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module my_concurrency
 
 go 1.19
+
+require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
